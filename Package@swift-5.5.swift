eaaf9b3531186b5390de3f558e58233d37cfb817
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-04-09 09:23:21 -0600

    Widened dependency constraints.

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -35,7 +35,7 @@ let package = Package(
 if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
     // Building standalone, so fetch all dependencies remotely.
     package.dependencies += [
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.0.1")),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.1"),
     ]
 
     package.targets += [
